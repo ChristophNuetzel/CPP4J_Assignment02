df4041ebdbc2a1b8d7cb1765490f33dbe73d4f2c
ChristophNuetzel <christoph_nuetzel@web.de>
2012-11-05 13:19:12 +0100

    project debugged - ; deleted :-)

--- C++4J_Assignment02/main.cpp
+++ C++4J_Assignment02/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello Assignment02!" << endl;
-    cout << "We shoul only work on the experiments branch! Merging into the master SHOULD ONLY BE DONE WHEN WE ARE IN THE SAME ROOM! ;-)"
+    cout << "We shoul only work on the experiments branch! Merging into the master SHOULD ONLY BE DONE WHEN WE ARE IN THE SAME ROOM! ;-)";
     return 0;
 }
 
